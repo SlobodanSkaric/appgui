24bbdf7d3eed31e63543727ce853c206e32af2ca
Slobodan <slobodan.skaric@gmail.com>
2023-01-17 18:01:55 +0100

    Import bootstrap jquery and fontawesome

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './commponents/App/App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "jquery/dist/jquery.js";
+import "popper.js/dist/popper.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/react-fontawesome";
+import "@fortawesome/fontawesome-svg-core";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
