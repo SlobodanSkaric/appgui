4ca01637d8c94eedd9d49ab5d047e821503315ef
Slobodan <slobodan.skaric@gmail.com>
2023-01-17 18:22:54 +0100

    Create main menu component

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,14 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/react-fontawesome";
 import "@fortawesome/fontawesome-svg-core";
+import { ManeMenue } from "./commponents/ManeMenue/ManeMenue";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ManeMenue></ManeMenue> 
     <App />
   </React.StrictMode>
 );
